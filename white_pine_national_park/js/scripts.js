b2774153653f30f1c02a50ce867eab4bb4f8a074
Patrick_Leeming <as9340tp@go.minneapolis.edu>
2018-11-18 18:36:35 -0600

    Fixed all bugs in navigation

---
+++ white_pine_national_park/js/scripts.js
@@ -0,0 +1,12 @@
+function myFunction(){
+        $("#toggle1").addClass("blu");
+        }
+function myFunction2() {
+    $("#toggle1").removeClass("blu");
+}
+function myFunction3() {
+    $("#toggle2").addClass("blu");
+}
+function myFunction4() {
+    $("#toggle2").removeClass("blu");
+}
