dda40e6ddd6af60197cec3b7ba498e1785d24e66
Patrick_Leeming <as9340tp@go.minneapolis.edu>
2018-12-01 15:23:58 -0600

    Made most of my FAQ, just need to fill FAQ with actual questions and have a decent amount of questions

--- white_pine_national_park/js/scripts.js
+++ white_pine_national_park/js/scripts.js
@@ -1,28 +1,12 @@
-$(document).ready(function() {
-
-    $(".toggle-accordion").on("click", function() {
-        var accordionId = $(this).attr("accordion-id"),
-            numPanelOpen = $(accordionId + ' .collapse.in').length;
-
-        $(this).toggleClass("active");
-
-        if (numPanelOpen == 0) {
-            openAllPanels(accordionId);
-        } else {
-            closeAllPanels(accordionId);
-        }
-    })
-
-    openAllPanels = function(aId) {
-        console.log("setAllPanelOpen");
-        $(aId + ' .panel-collapse:not(".in")').collapse('show');
-    }
-    closeAllPanels = function(aId) {
-        console.log("setAllPanelclose");
-        $(aId + ' .panel-collapse.in').collapse('hide');
-    }
-
+$(document).ready(function(){
+    $("#shw").click(function(){
+        $(".collapse2").collapse('show');
+    });
+    $("#hde").click(function(){
+        $(".collapse2").collapse('hide');
+    });
 });
+
 function myFunction(){
         $("#toggle1").addClass("blu");
         }
