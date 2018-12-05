1ea879780c5882c331e98f7b22c9620fa77b67a4
Patrick Leeming <as9340tp@t4530-3-7.campus.minneapolis.edu>
2018-12-05 12:16:50 -0600

    added card images to site and submit button and made things dissapear and appear when needed

--- white_pine_national_park/js/scripts.js
+++ white_pine_national_park/js/scripts.js
@@ -1,18 +1,31 @@
 $(document).ready(function(){
-    $(".every").click(function () {
+    $("#every").click(function() {
         $("#notShow1").removeClass("d-nne");
         $("#notShow2").removeClass("d-nne");
+        $("#notShow3").removeClass("d-nne");
+        $("#notShow4").removeClass("d-nne");
+        $("#notShow5").removeClass("d-nne");
     });
-    $("#not_repeating").click(function () {
+    $("#not_repeating").click(function() {
         $("#notShow1").addClass("d-nne");
         $("#notShow2").addClass("d-nne");
+        $("#notShow3").addClass("d-nne");
+        $("#notShow4").addClass("d-nne");
+        $("#notShow5").addClass("d-nne");
     });
-    $("#shw").click(function(){
+    $("#other").click(function() {
+        $("#price").removeClass("d-nne");
+    });
+    $(".dsply").click(function(){
+        $("#price").addClass("d-nne");
+    });
+    $("#shw").click(function() {
         $(".collapse2").collapse('show');
     });
     $("#hde").click(function(){
         $(".collapse2").collapse('hide');
     });
+    
 });
 
 function myFunction(){
